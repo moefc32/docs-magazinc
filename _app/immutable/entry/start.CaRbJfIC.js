68dff6cd99d750c755c809d15bccfe80d9ef1c21
Faizal Chan. <moefc32@gmail.com>
2025-04-15 08:19:21 +0700

    Updates

--- _app/immutable/entry/start.CaRbJfIC.js
+++
@@ -1,1 +0,0 @@
-import{l as o,a as r}from"../chunks/zLY87ZVS.js";export{o as load_css,r as start};
--- _app/immutable/nodes/2.BWDgAe0d.js
+++ _app/immutable/nodes/2.BWDgAe0d.js
@@ -1,4 +1,4 @@
-import{t as r,h as m,a as i,s as u}from"../chunks/Bol9LUmm.js";import{i as f}from"../chunks/11qMpu-4.js";import{p as h,t as s,a as g,$ as v,c as n,r as o,n as x}from"../chunks/B1iluEif.js";import{s as y}from"../chunks/BrogrN2b.js";var w=r('<meta property="og:title">'),b=r(`<div class="flex flex-1 flex-col gap-3"><h1 class="my-3 pb-3 text-5xl border-b-[1px] border-gray-700"> </h1> <p>Simpledocs is a minimalistic and efficient documentation site designed
+import{t as r,h as m,a as i,s as u}from"../chunks/CkPMGfKk.js";import{i as f}from"../chunks/ClzKPE17.js";import{p as h,t as s,a as g,$ as v,c as n,r as o,n as x}from"../chunks/BYOHr42B.js";import{s as y}from"../chunks/Czmk2fT1.js";var w=r('<meta property="og:title">'),b=r(`<div class="flex flex-1 flex-col gap-3"><h1 class="my-3 pb-3 text-5xl border-b-[1px] border-gray-700"> </h1> <p>Simpledocs is a minimalistic and efficient documentation site designed
         for designers, developers, and teams who need a quick and organized way
         to present their documentation. It offers a clean and responsive layout
         with essential features like a navigation sidebar and search
