ca3b26cfca0126c0a5deee6d435f3690e031cae8
Faizal Chan. <moefc32@gmail.com>
2026-02-13 17:47:23 +0700

    Updates

--- _app/immutable/nodes/2.B6afo-Rf.js
+++
@@ -1,8 +0,0 @@
-import"../chunks/Bzak7iHL.js";import{i as m}from"../chunks/CuuK_G7L.js";import{p as u,f as r,h as f,t as i,a as s,b as h,$ as g,c as n,r as o,n as v,s as x}from"../chunks/Bp7gmpXD.js";import{s as y}from"../chunks/hS5PVg1h.js";var b=r('<meta property="og:title"/>'),w=r(`<div class="flex flex-1 flex-col gap-3"><h1 class="my-3 pb-3 text-5xl border-b-[1px] border-gray-700"> </h1> <p>Simpledocs is a minimalistic and efficient documentation site designed
-        for designers, developers, and teams who need a quick and organized way
-        to present their documentation. It offers a clean and responsive layout
-        with essential features like a navigation sidebar and search
-        functionality. Built with simplicity in mind, this project ensures that
-        users can focus on writing content without dealing with complex
-        configurations. Its lightweight nature makes it easy to deploy on the
-        GitHub Pages.</p></div>`);function k(d,l){u(l,!1),m();var e=w();f(p=>{var a=b();i(()=>{g.title="Magazinc! Brand Guidelines",y(a,"content","Magazinc! Brand Guidelines")}),s(p,a)});var t=n(e),c=n(t,!0);o(t),v(2),o(e),i(()=>x(c,"Magazinc! Brand Guidelines")),s(d,e),h()}export{k as component};
